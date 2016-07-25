b4aac3c3d2d76856838d574d3557d3b53404532a
sohan <sohan.shetty@robosoftin.com>
2016-07-25 14:48:08 +0530

    made changes

--- app/src/main/java/com/example/nearbyplaces/MapsActivity.java
+++ app/src/main/java/com/example/nearbyplaces/MapsActivity.java
@@ -31,7 +31,6 @@ public class MapsActivity extends FragmentActivity implements NetworkAdapter.Net
                     mLocation = location;
                     Log.d(TAG, "Location ");
                     if (mLocation != null) {
-                        //logjkj
                         fetchNearByPlaces("");
                     } else {
                         Toast.makeText(MapsActivity.this, "wait untill fetching location complete", Toast.LENGTH_SHORT).show();
